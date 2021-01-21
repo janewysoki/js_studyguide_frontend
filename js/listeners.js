69522e2493ab4a5b503d6b71b35ef655674c0b3f
janewysoki <jane.wysoki@gmail.com>
2021-01-21 15:52:07 -0500

    rebuild event listener to submit new study guide

--- js/listeners.js
+++ js/listeners.js
@@ -4,31 +4,43 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 document.addEventListener('click', function(e) {
     console.dir(e.target)
-    let target = e.target;
-    if (target.matches('.editStudyGuide')) {
-        let guide = StudyGuide.findbyId(target.dataset.studyGuideId);
-        guide.edit();
-    }
 })
 
 document.addEventListener('submit', function(e) {
-    let target = e.target; 
+    let target = e.target;
     if(target.matches('#newStudyGuide')) {
-      e.preventDefault();
-      let nameInput = target.querySelector('input[name="name"]');
-      let formData = {
-        name: nameInput.value
-      };
-      StudyGuide.create({study_guide: formData})
-        .then(() => nameInput.value = "");
-    } else if (target.matches('.editStudyGuideForm')) {
-      e.preventDefault();
-      let nameInput = target.querySelector('input[name="name"]');
-      let formData = {
-        name: nameInput.value
-      };
-      let guide = StudyGuide.findById(target.dataset.studyGuideId);
-      guide.update({study_guide: formData});
-  
+        e.preventDefault(); //prevents page refresh?
+        console.log("submit form")
     }
-  })
+
+})
+// document.addEventListener('click', function(e) {
+//     console.dir(e.target)
+//     let target = e.target;
+//     if (target.matches('.editStudyGuide')) {
+//         let guide = StudyGuide.findbyId(target.dataset.studyGuideId);
+//         guide.edit();
+//     }
+// })
+
+// document.addEventListener('submit', function(e) {
+//     let target = e.target; 
+//     if(target.matches('#newStudyGuide')) {
+//       e.preventDefault();
+//       let nameInput = target.querySelector('input[name="name"]');
+//       let formData = {
+//           name: nameInput.value
+//       };
+//       StudyGuide.create({study_guide: formData})
+//         .then(() => nameInput.value = "");
+//     } else if (target.matches('.editStudyGuideForm')) {
+//       e.preventDefault();
+//       let nameInput = target.querySelector('input[name="name"]');
+//       let formData = {
+//         name: nameInput.value
+//       };
+//       let guide = StudyGuide.findById(target.dataset.studyGuideId);
+//       guide.update({study_guide: formData});
+  
+//     }
+// })
