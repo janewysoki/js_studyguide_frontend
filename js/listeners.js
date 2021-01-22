18e8c16322dd98904173f49537ff7964adeaafc2
janewysoki <jane.wysoki@gmail.com>
2021-01-21 19:38:18 -0500

    revise submit event listener

--- js/listeners.js
+++ js/listeners.js
@@ -10,37 +10,11 @@ document.addEventListener('submit', function(e) {
     let target = e.target;
     if(target.matches('#newStudyGuide')) {
         e.preventDefault(); //prevents page refresh?
-        console.log("submit form")
+        let formData = {}
+        target.querySelectorAll('input').forEach(function(input) {
+            formData[input.name] = input.value;
+        })
+        StudyGuide.create(formData);
     }
 
 })
-// document.addEventListener('click', function(e) {
-//     console.dir(e.target)
-//     let target = e.target;
-//     if (target.matches('.editStudyGuide')) {
-//         let guide = StudyGuide.findbyId(target.dataset.studyGuideId);
-//         guide.edit();
-//     }
-// })
-
-// document.addEventListener('submit', function(e) {
-//     let target = e.target; 
-//     if(target.matches('#newStudyGuide')) {
-//       e.preventDefault();
-//       let nameInput = target.querySelector('input[name="name"]');
-//       let formData = {
-//           name: nameInput.value
-//       };
-//       StudyGuide.create({study_guide: formData})
-//         .then(() => nameInput.value = "");
-//     } else if (target.matches('.editStudyGuideForm')) {
-//       e.preventDefault();
-//       let nameInput = target.querySelector('input[name="name"]');
-//       let formData = {
-//         name: nameInput.value
-//       };
-//       let guide = StudyGuide.findById(target.dataset.studyGuideId);
-//       guide.update({study_guide: formData});
-  
-//     }
-// })
